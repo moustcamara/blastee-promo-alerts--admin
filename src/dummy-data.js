0ea66e37b2b2a34317abf00adc9956a3cdd28c4f
Moust Camara <moust@moustcamara.com>
2019-06-22 18:12:27 -0400

    Installed 'Create React App'

---
+++ src/dummy-data.js
@@ -0,0 +1,44 @@
+let users = [
+
+	{
+		id: 'user1',
+		username: 'mike-smith',
+		name: 'Mike Smith'
+	}
+
+];
+
+let userWidgets = [
+
+	{
+		id: '9271',
+		title: 'Spring Sale',		
+		ownerId: 'user1',
+		ownerUsername: 'mike-smith'
+		widgetType: 'Pop Up',
+		creationDate: 'September 16, 2018',
+		status: 'active',
+	},
+
+	{
+		id: '2891',	
+		title: 'End of Season Sale',	
+		ownerId: 'user1',
+		ownerUsername: 'mike-smith'
+		widgetType: 'Pop Up',
+		creationDate: 'September 20, 2018',
+		status: 'active',
+	},
+
+	{
+		id: '2791',	
+		title: '2018 New Years Eve Sale: 50% Off',	
+		ownerId: 'user1',
+		ownerUsername: 'mike-smith'
+		widgetType: 'Pop Up',
+		creationDate: 'December 20, 2017',
+		status: 'active',
+	}
+
+
+]
