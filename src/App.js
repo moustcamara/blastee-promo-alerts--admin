218919681a3832b1dd3858615e8114e7b8ffbad3
moustcamara <moust@moustcamara.com>
2019-07-06 22:30:15 -0400

    Commented out the Firebase 'initializeApp' line of code

--- src/App.js
+++ src/App.js
@@ -1,168 +1,172 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import WidgetList from './components/WidgetList';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import WidgetList from "./components/WidgetList";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 //var app = firebase.initializeApp({ ... });
 
 //const firebase = require("../firebase");
 //require("../firebase/firestore");
 
 // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyB2It6JcF8rnO3iR_6i0gpD8szFOs4Q6y8",
-    authDomain: "blastee-promo-alerts-admin.firebaseapp.com",
-    databaseURL: "https://blastee-promo-alerts-admin.firebaseio.com",
-    projectId: "blastee-promo-alerts-admin",
-    storageBucket: "blastee-promo-alerts-admin.appspot.com",
-    messagingSenderId: "692010746936",
-    appId: "1:692010746936:web:36894fed8da275e1"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyB2It6JcF8rnO3iR_6i0gpD8szFOs4Q6y8",
+  authDomain: "blastee-promo-alerts-admin.firebaseapp.com",
+  databaseURL: "https://blastee-promo-alerts-admin.firebaseio.com",
+  projectId: "blastee-promo-alerts-admin",
+  storageBucket: "blastee-promo-alerts-admin.appspot.com",
+  messagingSenderId: "692010746936",
+  appId: "1:692010746936:web:36894fed8da275e1"
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+// Remove comment lines for code below when launching app locally (or in Firebase)
+//firebase.initializeApp(firebaseConfig);
 
-  var db = firebase.firestore();
+var db = firebase.firestore();
 
 //import dummyData from 'dummy-data.js'; */
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: 'user1',
+      currentUser: "user1",
       widgets: []
-    }
+    };
   }
 
   componentDidMount() {
-
     let userWidgets = [];
 
-    let getWidgets = db.collection('widgets');
-    let widgetQuery = getWidgets.where('ownerId', '==', this.state.currentUser).get()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        console.log('No matching documents.');
-        return;
-      }  
-      snapshot.forEach(doc => {
-        userWidgets.push(doc.data());
-        
-      });
+    let getWidgets = db.collection("widgets");
+    let widgetQuery = getWidgets
+      .where("ownerId", "==", this.state.currentUser)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+          return;
+        }
+        snapshot.forEach(doc => {
+          userWidgets.push(doc.data());
+        });
 
-      this.setState({
+        this.setState({
           widgets: userWidgets
         });
 
-      //return this.state.widgets;
-
-    })
-    .catch(err => {
-      console.log('Error getting documents', err);
-    });
-
- 
-
+        //return this.state.widgets;
+      })
+      .catch(err => {
+        console.log("Error getting documents", err);
+      });
   }
 
+  render() {
+    let users = [
+      {
+        id: "user1",
+        username: "mike-smith",
+        name: "Mike Smith"
+      }
+    ];
 
-  render(){
+    let uw = [
+      {
+        title: "Spring Sale",
+        ownerId: "user1",
+        ownerUsername: "mike-smith",
+        widgetType: "Pop Up",
+        creationDate: "September 16, 2018",
+        status: "active"
+      },
 
+      {
+        title: "End of Season Sale",
+        ownerId: "user1",
+        ownerUsername: "mike-smith",
+        widgetType: "Banner",
+        creationDate: "September 20, 2018",
+        status: "active"
+      },
 
-      let banana = 'a very nice fruit!';
+      {
+        title: "2018 New Years Eve Sale: 50% Off",
+        ownerId: "user1",
+        ownerUsername: "mike-smith",
+        widgetType: "Pop Up",
+        creationDate: "December 20, 2017",
+        status: "not active"
+      }
+    ];
 
-      let users = [
-        {
-          id: 'user1',
-          username: 'mike-smith',
-          name: 'Mike Smith'
-        }
-      ];
+    let addUser = (firstName, lastName) => {
+      let username = [firstName, lastName].join("-").toLowerCase();
+      return db.collection("users").add({
+        firstName,
+        lastName,
+        username
+      });
+    };
 
- let uw = [
-        {
-          title: 'Spring Sale',   
-          ownerId: 'user1',
-          ownerUsername: 'mike-smith',
-          widgetType: 'Pop Up',
-          creationDate: 'September 16, 2018',
-          status: 'active',
-        },
+    let widgetCounter = 0;
 
-        {
-          title: 'End of Season Sale',  
-          ownerId: 'user1',
-          ownerUsername: 'mike-smith',
-          widgetType: 'Banner',
-          creationDate: 'September 20, 2018',
-          status: 'active',
-        },
-
-        {
-          title: '2018 New Years Eve Sale: 50% Off',  
-          ownerId: 'user1',
-          ownerUsername: 'mike-smith',
-          widgetType: 'Pop Up',
-          creationDate: 'December 20, 2017',
-          status: 'not active',
-        } 
-    ]
-
-
-      let addUser = (firstName, lastName) => {
-        let username = [firstName,lastName].join('-').toLowerCase();
-        return(
-          db.collection("users").add({
-                firstName,
-                lastName,
-                username 
-          })
-        )
-      };
-
-      let widgetCounter = 0;
-
-      db.collection("counters").doc("stats").get().then(function(doc) {
-           widgetCounter = doc.data().widgets;
+    db.collection("counters")
+      .doc("stats")
+      .get()
+      .then(function(doc) {
+        widgetCounter = doc.data().widgets;
       });
 
-      let addMultipleWidgets = (widgets) => {
-        return(
-          uw.map(singleWidget => {
-            db.collection("counters").doc("stats").set({widgets: ++widgetCounter});
-            db.collection("widgets").doc('bcpa'.concat(widgetCounter)).set(singleWidget)
-          })
-        )
-      };
+    let addMultipleWidgets = widgets => {
+      return uw.map(singleWidget => {
+        db.collection("counters")
+          .doc("stats")
+          .set({ widgets: ++widgetCounter });
+        db.collection("widgets")
+          .doc("bcpa".concat(widgetCounter))
+          .set(singleWidget);
+      });
+    };
 
-      return (
-
-        <div className="App">
-          <Header/>
-          <div className="main">  
-            <button onClick={() => alert(widgetCounter)}>Count data</button>
-            <button onClick={() => addMultipleWidgets(uw)} >Add Sample Widgets</button>      
-            <button onClick={() => addUser('Jenna', 'McKenzie')} >Add User</button>
-            <button onClick={() => db.collection("users").doc("rob-hueman").delete()} >Delete a user</button>
-            <WidgetList title="Alerts" user={users.find(x => x.id == this.state.currentUser).name} dataSource={this.state.widgets} />
+    return (
+      <div className="App">
+        <Header />
+        <div className="main">
+          <button onClick={() => alert(widgetCounter)}>Count data</button>
+          <button onClick={() => addMultipleWidgets(uw)}>
+            Add Sample Widgets
+          </button>
+          <button onClick={() => addUser("Jenna", "McKenzie")}>Add User</button>
+          <button
+            onClick={() =>
+              db
+                .collection("users")
+                .doc("rob-hueman")
+                .delete()
             }
-          </div>                 
-          <Footer />
+          >
+            Delete a user
+          </button>
+          <WidgetList
+            title="Alerts"
+            user={users.find(x => x.id == this.state.currentUser).name}
+            dataSource={this.state.widgets}
+          />
+          }
         </div>
-      );
-
+        <Footer />
+      </div>
+    );
   }
-
-  
 }
 
 export default App;
